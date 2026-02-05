4a6338f66f602b6a7f09e9b7ae77e4da058a0e44
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-02-05 17:25:29 +0600

    few update

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+namespace ConsoleAppTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
