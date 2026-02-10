e7e693147979f0e557c66fe00952a052dd2c19ab
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-02-10 10:52:59 +0600

    switch

--- practice/Program.cs
+++ practice/Program.cs
@@ -2,16 +2,37 @@
 {
     private static void Main(string[] args)
     {
-        Console.Write("Enter your age: ");
-        int age = Convert.ToInt32(Console.ReadLine());
-        if(age >= 18)
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        // if-else
+        if (a == 1 || a == 2)
         {
-            Console.WriteLine("You are an adult.");
+            Console.WriteLine("a is 1 or 2");
+        }
+        else if (a >= 3 && a <= 8)
+        {
+            Console.WriteLine("a is between 3 and 8");
         }
         else
         {
-            Console.WriteLine("You are not an adult.");
+            Console.WriteLine("a is not 1, 2, 3, or 4");
         }
 
+        // switch
+        switch (a)
+        {
+            case 1:
+            case 2:
+                Console.WriteLine("a is 1 or 2");
+                break;
+
+            case >= 3 and <= 8:
+                Console.WriteLine("a is between 3 and 8");
+                break;
+
+            default:
+                Console.WriteLine("a is not 1, 2, 3, or 4");
+                break;
+        }
     }
 }
