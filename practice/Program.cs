15a276f6c40437c9651b425dee0c14da40c10746
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-02-10 09:07:16 +0600

    if else

--- practice/Program.cs
+++ practice/Program.cs
@@ -2,10 +2,16 @@
 {
     private static void Main(string[] args)
     {
-        int age = 25;
-        Console.WriteLine($"I am {age} years old.");
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        if(age >= 18)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+        else
+        {
+            Console.WriteLine("You are not an adult.");
+        }
 
-        Console.WriteLine(int.MaxValue);
-        Console.WriteLine(int.MinValue);
     }
 }
