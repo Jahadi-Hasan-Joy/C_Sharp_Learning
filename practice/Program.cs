3a500a154c394382b4958cfc2ce40079f8812312
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-02-12 21:58:39 +0600

    string

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,38 +1,20 @@
-﻿internal class Program
+﻿using System;
+using System.Globalization;
+
+namespace Complete_CSharp_Guide
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        int a = Convert.ToInt32(Console.ReadLine());
-
-        // if-else
-        if (a == 1 || a == 2)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("a is 1 or 2");
-        }
-        else if (a >= 3 && a <= 8)
-        {
-            Console.WriteLine("a is between 3 and 8");
-        }
-        else
-        {
-            Console.WriteLine("a is not 1, 2, 3, or 4");
-        }
+            string name = "Md jahadi hasan joy";
 
-        // switch
-        switch (a)
-        {
-            case 1:
-            case 2:
-                Console.WriteLine("a is 1 or 2");
-                break;
-
-            case >= 3 and <= 8:
-                Console.WriteLine("a is between 3 and 8");
-                break;
-
-            default:
-                Console.WriteLine("a is not 1, 2, 3, or 4");
-                break;
+            for(int i = 0; i < name.Length; i++)
+            {
+                Console.Write(name[i]);
+                Thread.Sleep(100);
+            }
+                       
         }
     }
 }
