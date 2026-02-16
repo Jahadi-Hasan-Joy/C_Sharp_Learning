db07087b9b3f79e13e9a464e516052664b945cea
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-02-16 19:37:04 +0600

    arrary positon check

--- practice/Program.cs
+++ practice/Program.cs
@@ -7,13 +7,9 @@ namespace Complete_CSharp_Guide
     {
         private static void Main(string[] args)
         {
-            int[] num = new int[5] { 1, 2, 3, 4, 5 };
-            Array.Reverse(num);
-            foreach (int i in num)
-            {
-                Console.Write($"{i} ");
-            }
-
+            int[] num = new int[] {0, 1, 2, 3, 1 ,4, 5,6,7,8,9 };
+            int pos = Array.IndexOf(num, 1);
+            Console.WriteLine("poss = "+ pos);
             Console.ReadLine();
         }
     }
