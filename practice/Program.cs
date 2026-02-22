2bd6a230224b67e246a6ad7948a34f445c1ec457
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-02-22 10:28:55 +0600

    To generate Japanese invoices

--- practice/Program.cs
+++ practice/Program.cs
@@ -5,8 +5,12 @@ namespace Complete_CSharp_Guide
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(@"    c:\source\repos    
-        (this is where your code goes)");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // To generate Japanese invoices:
+            // Nihon no seikyū-sho o seisei suru ni wa:
+            Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+            Console.WriteLine(@"c:\invoices\app.exe -j");
             Console.WriteLine();
         }
     }
