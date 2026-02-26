30271854963ac5ae52147e34a8093c61b2727062
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-02-26 12:33:12 +0600

    Use tryParse to avoide occur erron in string to int conversion

--- BasicConsoleApp/BasicConsoleApp/Program.cs
+++ BasicConsoleApp/BasicConsoleApp/Program.cs
@@ -11,12 +11,9 @@ namespace BasicConsoleApp
     {
         static void Main(string[] args)
         {
-            double a = 5.0 / 2.3;
-            Console.WriteLine(a);
-            Console.WriteLine(string.Format("{0:0.00}",a));
-            Console.WriteLine(a.ToString("C"));
-            Console.WriteLine(a.ToString("C",CultureInfo.CreateSpecificCulture("en-GB")));
-
+            string snum = Console.ReadLine();
+            int.TryParse(snum, out int num);
+            Console.WriteLine(num);
         }
         
     }
