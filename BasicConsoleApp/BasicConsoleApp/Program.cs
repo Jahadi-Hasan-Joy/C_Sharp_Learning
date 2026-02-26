ea70fe3e1c57eb6f5db955c74ba70908aa595c2d
Md Jahadi Hasan Joy <mdjihadihasan2551@gmail.com>
2026-02-26 12:05:34 +0600

    Create Specific Culture for euro currency

--- BasicConsoleApp/BasicConsoleApp/Program.cs
+++ BasicConsoleApp/BasicConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,12 @@ namespace BasicConsoleApp
     {
         static void Main(string[] args)
         {
-            var a = 10.0 / 4;
+            double a = 5.0 / 2.3;
             Console.WriteLine(a);
+            Console.WriteLine(string.Format("{0:0.00}",a));
+            Console.WriteLine(a.ToString("C"));
+            Console.WriteLine(a.ToString("C",CultureInfo.CreateSpecificCulture("en-GB")));
+
         }
         
     }
